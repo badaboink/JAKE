4d0d1dc043daa1c108d1194c9ca53e9df98ad767
Eglė Janušaityė <eglejanusaityte@gmail.com>
2023-09-15 18:56:23 +0300

    zvng

---
+++ JAKE.classlibrary/Obstacle.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JAKE.classlibrary
+{
+    public class Obstacle
+    {
+        public double Width { get; set; }
+        public double Height { get; set; }
+        public double PositionX { get; set; }
+        public double PositionY { get; set; }
+
+        public Obstacle(double width, double height, double positionX, double positionY)
+        {
+            Width = width;
+            Height = height;
+            PositionX = positionX;
+            PositionY = positionY;
+        }
+
+        public bool WouldOverlap(double newX, double newY, double playerWidth, double playerHeight)
+        {
+            // Calculate the boundaries of the obstacle
+            double obstacleLeft = PositionX;
+            double obstacleRight = PositionX + Width;
+            double obstacleTop = PositionY;
+            double obstacleBottom = PositionY + Height;
+
+            // Calculate the boundaries of the new position
+            double newLeft = newX;
+            double newRight = newX + playerWidth;
+            double newTop = newY;
+            double newBottom = newY + playerHeight;
+
+            // Check for overlap
+            bool overlapX = newLeft < obstacleRight && newRight > obstacleLeft;
+            bool overlapY = newTop < obstacleBottom && newBottom > obstacleTop;
+
+            return overlapX && overlapY;
+        }
+        public override string ToString()
+        {
+            return $"{Width}:{Height}:{PositionX}:{PositionY}";
+        }
+    }
+}
---
+++ JAKE.classlibrary/Player.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JAKE.classlibrary
+{
+    public class Player
+    {
+        private int _id;
+        private string _name;
+        private string _color;
+        private double _currentX;
+        private double _currentY;
+        public string Ability { get; set; }
+
+        public Player(int id, string name, string color)
+        {
+            _id = id;
+            _name = name;
+            _color = color;
+            SetCurrentPosition(0, 0);
+            if (ColorToAbilityMap.TryGetValue(color, out var ability))
+            {
+                Ability = ability;
+            }
+            else
+            {
+                Ability = "unknown";
+            }
+        }
+
+        public int GetId()
+        {
+            return _id;
+        }
+
+        public string GetName()
+        {
+            return _name;
+        }
+
+        public string GetColor()
+        {
+            return _color;
+        }
+
+        public double GetCurrentX()
+        {
+            return _currentX;
+        }
+
+        public double GetCurrentY()
+        {
+            return _currentY;
+        }
+
+        public void SetCurrentPosition(double x, double y)
+        {
+            _currentX = x;
+            _currentY = y;
+        }
+        private static readonly Dictionary<string, string> ColorToAbilityMap = new Dictionary<string, string>
+        {
+            { "Green", "heal" },
+            { "Blue", "wall" },
+            { "Red", "strength" },
+        };
+        public override bool Equals(object obj)
+        {
+            if (obj is Player otherPlayer)
+            {
+                return this.GetId() == otherPlayer.GetId();
+            }
+            return false;
+        }
+        public override int GetHashCode()
+        {
+            return this.GetId().GetHashCode();
+        }
+        public override string ToString()
+        {
+            return $"{GetId()}:{GetName()}:{GetColor()}:{GetCurrentX()}:{GetCurrentY()}";
+        }
+    }
+}
