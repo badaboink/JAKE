33a0d8dac308ccd80395ad0cc331a1488224206f
NodGod <joris.gagilas@gmail.com>
2023-10-22 18:07:59 +0300

    Added Command pattern and fixed some movement bugs

---
+++ JAKE.classlibrary/MovementController.cs
@@ -0,0 +1,30 @@
+﻿using JAKE.classlibrary.Patterns;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace JAKE.classlibrary
+{
+    public class Controller
+    {
+        Command slot;
+
+        public Controller()
+        {
+
+        }
+
+        public void SetCommand(Command command)
+        {
+            slot = command;
+        }
+
+        public void Execute()
+        {
+            slot.Execute();
+        }
+    }
+}
