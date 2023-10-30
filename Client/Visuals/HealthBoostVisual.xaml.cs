ba52d4eb93fa629ced32145159081d99312dcfda
Agnė Rauluševičiūtė <agne.rauluseviciute@ktu.edu>
2023-10-31 00:44:15 +0200

    delete visuals

--- Client/Visuals/HealthBoostVisual.xaml.cs
+++ Client/Visuals/HealthBoostVisual.xaml.cs
@@ -15,57 +15,13 @@ using System.Windows.Shapes;
 
 namespace JAKE.client
 {
-    /// <summary>
-    /// Interaction logic for HealthBoostVisual.xaml
-    /// </summary>
+    
     public partial class HealthBoostVisual : UserControl
     {
-        public string HealthBoostImageSource
-        {
-            get { return (string)GetValue(HealthBoostImageSourceProperty); }
-            set { SetValue(HealthBoostImageSourceProperty, value); }
-        }
-
-        // Using a DependencyProperty as the backing store for HealthBoostImageSource. 
-        // This enables animation, styling, binding, etc...
-        public static readonly DependencyProperty HealthBoostImageSourceProperty =
-            DependencyProperty.Register("HealthBoostImageSource", typeof(string),
-                                        typeof(HealthBoostVisual), new PropertyMetadata(null));
-
-        public int HealthBoostImageHeight
-        {
-            get { return (int)GetValue(HealthBoostImageHeightProperty); }
-            set { SetValue(HealthBoostImageHeightProperty, value); }
-        }
-
-        // Property for the HealthBoost image width
-        public int HealthBoostImageWidth
-        {
-            get { return (int)GetValue(HealthBoostImageWidthProperty); }
-            set { SetValue(HealthBoostImageWidthProperty, value); }
-        }
-
-        // Dependency properties for binding in XAML
-        public static readonly DependencyProperty HealthBoostImageHeightProperty =
-            DependencyProperty.Register("HealthBoostImageHeight", typeof(int), typeof(HealthBoostVisual), new PropertyMetadata(15));
-
-        public static readonly DependencyProperty HealthBoostImageWidthProperty =
-            DependencyProperty.Register("HealthBoostImageWidth", typeof(int), typeof(HealthBoostVisual), new PropertyMetadata(15));
-
-        public HealthBoostVisual(string image, int width, int height)
-        {
-            InitializeComponent();
-            HealthBoostImageHeight = height;
-            HealthBoostImageWidth = width;
-            HealthBoostImageSource = image;
-        }
-
         public HealthBoostVisual()
         {
             InitializeComponent();
-           // healthBoostCircle.Fill = new SolidColorBrush(Colors.Pink);
         }
-
 
     }
 }
