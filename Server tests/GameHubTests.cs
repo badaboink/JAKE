e714caa1403d2f46fe259ec6fd7fa0b4aab6940a
Eglė Janušaityė <eglejanusaityte@gmail.com>
2023-10-30 11:46:52 +0200

    Fixed tests and builders tests

---
+++ Server tests/GameHubTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Connections;
+using Microsoft.AspNetCore.SignalR;
+using Moq;
+using Server.GameData;
+using Server.Hubs;
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace Server_tests
+{
+    public class GameHubTests
+    {
+        [Fact]
+        public async Task OnConnectedAsync_Should_AddObserverToGameData()
+        {
+
+        }
+    }
+}
